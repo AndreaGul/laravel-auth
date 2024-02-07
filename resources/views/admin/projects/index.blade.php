3da31b073e806d923e933e5865cb255b429f67cc
AndreaGul <andreagulino99@gmail.com>
2024-02-07 17:37:05 +0100

    feat: grafica pagina progetti e show dei progetti

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,15 +1,38 @@
 @extends('layouts.admin')
 
 @section('content')
-  <ul >
+  
+
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Titolo</th>
+      <th scope="col">Autore</th>
+      <th scope="col">Linguaggio</th>
+      <th class="text-center" scope="col">Link progetto</th>
+      <th class="text-center" scope="col">info</th>
+    </tr>
+  </thead>
+  <tbody>
     @foreach ($projects as $project)
-        <li>Titolo: {{$project->title}}</li>
-        <li>Autore: {{$project->author}}</li>
-        <li>Data creazione: {{$project->creation_date}}</li>
-        <li>Ultima commit: {{$project->last_update}}</li>
-        <li>Linguaggi di programmazione: {{$project->lang}}</li>
-        <li>Link: {{$project->link_github}}</li>
-        <br>
+    <tr class="align-middle">
+      <th scope="row">{{$project->id}}</th>
+      <td>{{$project->title}}</td>
+      <td>{{$project->title}}</td>
+      <td>{{$project->lang}}</td>
+      <td class="text-center">
+        <button class="btn  btn-primary btn-success ">
+          <a class="text-white text-decoration-none " href="{{$project->link_github}}"><i class="fa-brands fa-github"></i></a>
+        </button>
+      </td>
+      <td class="text-center" >
+        <button class="btn btn-primary btn-info">
+          <a class="text-white text-decoration-none" href="{{ route('admin.projects.show', $project->id)}}">Info</a>
+        </button>
+      </td>
+    </tr>
     @endforeach
-  </ul>
+  </tbody>
+</table>
 @endsection
